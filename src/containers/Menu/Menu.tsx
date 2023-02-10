d4f4b02575ac72ebb22a9a7dc79aeeac514fca46
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-02-10 11:58:33 +0300

    Add menu items

--- src/containers/Menu/Menu.tsx
+++ src/containers/Menu/Menu.tsx
@@ -1,10 +1,15 @@
 import { memo } from "react"
+import MenuItem from "../../components/MenuItem/MenuItem"
 import styles from './Menu.module.scss'
 
 function Menu () {
     return (
         <section className={styles.menu}>
-
+            <MenuItem href="#">главная</MenuItem>
+            <MenuItem href="#">каталог</MenuItem>
+            <MenuItem href="#">проекты</MenuItem>
+            <MenuItem href="#">контакты</MenuItem>
+            <MenuItem href="#">FAQ</MenuItem>
         </section>
     )
 }
